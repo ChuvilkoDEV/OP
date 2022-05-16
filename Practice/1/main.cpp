a2a7fbd7d47d5b4c3bddc2c4fd098b59fc1e47b5
ChuvilkoDEV <ilyasutton@gmail.com>
2022-05-16 20:29:47 +0300

    added: tests

--- Practice/1/main.cpp
+++ Practice/1/main.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 
 #define EPS 0.01
+#define DEBUG
 
 // Сложность O(n)
 double deltaPrice_kiss(int n, double p) {
@@ -19,22 +20,45 @@ double deltaPrice_formula(int n, double p) {
   return 100 - pow(1 - pow(p, 2) / 10000, n) * 100;
 }
 
-void tests() {
+void test_default() {
   assert(abs(deltaPrice_formula(1, 10) - 1) <= EPS);
+}
+
+void test_zeroPercent() {
   assert(abs(deltaPrice_formula(1, 0)) <= EPS);
+}
+
+void test_twoYear() {
   assert(abs(deltaPrice_formula(2, 10) - 1.99) <= EPS);
+}
+
+void test_oneHundredPercent() {
   assert(abs(deltaPrice_formula(2, 100) - 100) <= EPS);
+}
+
+void test_bigPercent() {
   assert(abs(deltaPrice_formula(3, 156) - 394.635) <= EPS);
 }
 
+void tests() {
+  test_default();
+  test_zeroPercent();
+  test_twoYear();
+  test_oneHundredPercent();
+  test_bigPercent();
+}
+
 int main() {
+#ifdef DEBUG
   tests();
-/*
+#endif
+#ifndef DEBUG
   int n;
   double p;
   std::cin >> n >> p;
 
   std::cout << "The price will decrease by " << deltaPrice_formula(n, p) << "%";
-*/
+#endif
+
   return 0;
 }
