fc93e63d94d5f968eca1a4899e7a581609ef3ece
ChuvilkoDEV <ilyasutton@gmail.com>
2022-05-25 15:56:29 +0300

    Добавил функции для материальной точки

--- Lab 6/f/MaterialPoint/MaterialPoint.cpp
+++ Lab 6/f/MaterialPoint/MaterialPoint.cpp
@@ -4,3 +4,24 @@ MaterialPoint::MaterialPoint(double speed, double acceleration) {
   _speed = speed;
   _acceleration = acceleration;
 }
+
+void MaterialPoint::setSpeed(double speed) {
+  _speed = speed;
+}
+
+void MaterialPoint::setAcceleration(double acceleration) {
+  _acceleration = acceleration;
+}
+
+double MaterialPoint::getPosition() const {
+  return _position;
+}
+
+double MaterialPoint::getSpeed() const {
+  return _speed;
+}
+
+void MaterialPoint::move(int duration) {
+  _position += duration * _speed + _acceleration * pow(duration, 2) / 2;
+  _speed += duration * _acceleration;
+}
--- Lab 6/f/MaterialPoint/MaterialPoint.h
+++ Lab 6/f/MaterialPoint/MaterialPoint.h
@@ -2,16 +2,26 @@
 #define F_MATERIALPOINT_H
 
 #include <iostream>
-
+#include <cmath>
 
 class MaterialPoint {
-  private:
+private:
   double _position = 0;       // Положение материальной точки
   double _speed;         // Скорость материальной точки
   double _acceleration;  // Ускорение материальной точки
 
-  public:
-  MaterialPoint(double speed = 0, double acceleration = 0);
+public:
+  explicit MaterialPoint(double speed = 0, double acceleration = 0);
+
+  void setSpeed(double speed);
+
+  void setAcceleration(double acceleration);
+
+  double getPosition() const;
+
+  double getSpeed() const;
+
+  void move(int duration);
 };
 
 #endif //F_MATERIALPOINT_H
